421883702c503c9ac945459348e212b7f547722f
Luís Narciso <Lus Narciso@LNarciso>
2017-03-20 19:56:29 +0000

    w56i35k6

--- src/view/GuiCliente.java
+++ src/view/GuiCliente.java
@@ -1,9 +1,34 @@
 package view;
 
+import java.util.Scanner;
+
+import model.Cliente;
+
 public class GuiCliente {
+
+	private Scanner scanner;
 
 	public GuiCliente() {
 		
 	}
+	
+public Cliente criarCliente(){
+		
+		System.out.println("Introduza o nome do cliente");
+		scanner = new Scanner(System.in);
+		String nome = scanner.nextLine();
+		
+		System.out.println("Introduza o contato do cliente");
+		String contato = scanner.nextLine();
+		
+		Cliente c = new Cliente( nome, contato, contato, contato, 0);
+		c.setNome(nome);
 
+		
+		return c;
+		
+	}
+	
 }
+
+
