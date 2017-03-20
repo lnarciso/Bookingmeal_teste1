353a01d94e48be270583b887031002604e1579a6
Luís Narciso <Lus Narciso@LNarciso>
2017-03-20 18:05:43 +0000

    app

--- src/application/App.java
+++ src/application/App.java
@@ -2,8 +2,10 @@ package application;
 
 public class App {
 
-	public App() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
 	}
 
 }
