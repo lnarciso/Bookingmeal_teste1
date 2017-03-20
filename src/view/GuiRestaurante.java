bebb23a3420e19dceba5d2630f562c15242c99ea
Luís Narciso <Lus Narciso@LNarciso>
2017-03-20 20:00:56 +0000

    xewcevevev

--- src/view/GuiRestaurante.java
+++ src/view/GuiRestaurante.java
@@ -1,9 +1,32 @@
 package view;
 
+import java.util.Scanner;
+
+import model.Restaurante;
+
 public class GuiRestaurante {
 
-	public GuiRestaurante() {
-		// TODO Auto-generated constructor stub
+	
+	public GuiRestaurante (){
+		
+	}//end GuiVendedor
+	
+	public Restaurante criarRestaurante(){
+		
+		System.out.println("Introduza o nome do restaurante");
+		String nome = new Scanner(System.in).nextLine();
+		
+		System.out.println("Introduza a categoria do restaurante (1-3)");
+		String cat = new Scanner(System.in).nextLine();
+		
+		Restaurante r = new Restaurante(nome, nome, nome, nome, cat);
+		r.setNome(nome);
+		r.setCategoria(cat);
+		
+		
+	
+		return r;
+		
 	}
 
 }
